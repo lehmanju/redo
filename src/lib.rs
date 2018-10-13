2ed8a96d6802caabc880952734ddaae050a30a2c
evenorog <evenorog@gmail.com>
2018-10-13 21:53:58 +0200

    0.28.0 -> 0.28.1

--- src/lib.rs
+++ src/lib.rs
@@ -17,7 +17,7 @@
 //! [Display]: struct.Display.html
 //! [`merge`]: trait.Command.html#method.merge
 
-#![doc(html_root_url = "https://docs.rs/redo/0.28.0")]
+#![doc(html_root_url = "https://docs.rs/redo/0.28.1")]
 #![deny(
     bad_style,
     bare_trait_objects,
