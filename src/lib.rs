aedcbfd4bcd7d574aac42ee753e4e968b0f3f87b
evenorog <evenorog@gmail.com>
2019-01-15 15:33:42 +0100

    0.30.0 -> 0.30.1

--- src/lib.rs
+++ src/lib.rs
@@ -68,7 +68,7 @@
 //! [Display]: struct.Display.html
 //! [merge]: trait.Command.html#method.merge
 
-#![doc(html_root_url = "https://docs.rs/redo/0.30.0")]
+#![doc(html_root_url = "https://docs.rs/redo/0.30.1")]
 #![deny(
     bad_style,
     bare_trait_objects,
