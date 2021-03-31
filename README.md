d9597cd4efa74a49dde6b0274da6424c4d8086e7
evenorog <evenorog@gmail.com>
2021-03-31 12:30:39 +0200

    Fix serde feature

--- README.md
+++ README.md
@@ -11,69 +11,6 @@ by creating objects of commands that applies the modifications. All commands kno
 how to undo the changes it applies, and by using the provided data structures
 it is easy to apply, undo, and redo changes made to a target.
 
-## Features
-
-* [Command](https://docs.rs/redo/latest/redo/trait.Command.html) provides the base functionality for all commands.
-* [Record](https://docs.rs/redo/latest/redo/struct.Record.html) provides basic linear undo-redo functionality.
-* [History](https://docs.rs/redo/latest/redo/struct.History.html) provides non-linear undo-redo functionality that allows you to jump between different branches.
-* Queue wraps a record or history and extends them with queue functionality.
-* Checkpoint wraps a record or history and extends them with checkpoint functionality.
-* Commands can be merged into a single command by implementing the 
-  [merge](https://docs.rs/redo/latest/redo/trait.Command.html#method.merge) method on the command.
-  This allows smaller commands to be used to build more complex operations, or smaller incremental changes to be
-  merged into larger changes that can be undone and redone in a single step.
-* The target can be marked as being saved to disk and the data-structures can track the saved state and notify
-  when it changes.
-* The amount of changes being tracked can be configured by the user so only the `N` most recent changes are stored.
-* Configurable display formatting using the display structure.
-* The library can be used as `no_std` by default.
-
-## Cargo Feature Flags
-
-* `chrono`: Enables time stamps and time travel.
-* `serde`: Enables serialization and deserialization.
-* `colored`: Enables colored output when visualizing the display structures.
-
-## Examples
-
-```rust
-use redo::{Command, Record};
-
-struct Add(char);
-
-impl Command for Add {
-    type Target = String;
-    type Error = &'static str;
-
-    fn apply(&mut self, s: &mut String) -> redo::Result<Add> {
-        s.push(self.0);
-        Ok(())
-    }
-
-    fn undo(&mut self, s: &mut String) -> redo::Result<Add> {
-        self.0 = s.pop().ok_or("s is empty")?;
-        Ok(())
-    }
-}
-
-fn main() -> redo::Result<Add> {
-    let mut record = Record::default();
-    record.apply(Add('a'))?;
-    record.apply(Add('b'))?;
-    record.apply(Add('c'))?;
-    assert_eq!(record.target(), "abc");
-    record.undo()?;
-    record.undo()?;
-    record.undo()?;
-    assert_eq!(record.target(), "");
-    record.redo()?;
-    record.redo()?;
-    record.redo()?;
-    assert_eq!(record.target(), "abc");
-    Ok(())
-}
-```
-
 ### License
 
 Licensed under either of
--- src/lib.rs
+++ src/lib.rs
@@ -4,54 +4,37 @@
 //! by creating objects of commands that applies the modifications. All commands knows
 //! how to undo the changes it applies, and by using the provided data structures
 //! it is easy to apply, undo, and redo changes made to a target.
-//!
-//! # Features
-//!
-//! * [Command](trait.Command.html) provides the base functionality for all commands.
-//! * [Record](struct.Record.html) provides basic linear undo-redo functionality.
-//! * [History](struct.History.html) provides non-linear undo-redo functionality that allows you to jump between different branches.
-//! * Queue wraps a record or history and extends them with queue functionality.
-//! * Checkpoint wraps a record or history and extends them with checkpoint functionality.
-//! * Commands can be merged into a single command by implementing the
-//!   [merge](trait.Command.html#method.merge) method on the command.
-//!   This allows smaller commands to be used to build more complex operations, or smaller incremental changes to be
-//!   merged into larger changes that can be undone and redone in a single step.
-//! * The target can be marked as being saved to disk and the data-structures can track the saved state and notify
-//!   when it changes.
-//! * The amount of changes being tracked can be configured by the user so only the `N` most recent changes are stored.
-//! * Configurable display formatting using the display structure.
-//! * The library can be used as `no_std` by default.
-//!
-//! # Cargo Feature Flags
-//!
-//! * `chrono`: Enables time stamps and time travel.
-//! * `serde`: Enables serialization and deserialization.
-//! * `colored`: Enables colored output when visualizing the display structures.
 
 #![doc(html_root_url = "https://docs.rs/redo")]
 #![deny(missing_docs)]
 #![forbid(unsafe_code)]
 
-use serde::{Deserialize, Serialize};
-use undo::Command;
-use undo::Record as Inner;
-use undo::Signal;
+#[cfg(feature = "serde")]
+use serde_crate::{Deserialize, Serialize};
+use undo::{Command, History as Inner, Signal};
 
 /// A history of commands.
-#[derive(Clone, Serialize, Deserialize)]
-#[serde(bound(
-    serialize = "C: Command + Serialize, C::Target: Serialize",
-    deserialize = "C: Command + Deserialize<'de>, C::Target: Deserialize<'de>"
-))]
-pub struct Record<C: Command, F = Box<dyn FnMut(Signal)>> {
+#[cfg_attr(
+    feature = "serde",
+    derive(Serialize, Deserialize),
+    serde(
+        crate = "serde_crate",
+        bound(
+            serialize = "C: Command + Serialize, C::Target: Serialize",
+            deserialize = "C: Command + Deserialize<'de>, C::Target: Deserialize<'de>"
+        )
+    )
+)]
+#[derive(Clone)]
+pub struct History<C: Command, F = Box<dyn FnMut(Signal)>> {
     inner: Inner<C, F>,
     target: C::Target,
 }
 
-impl<C: Command> Record<C> {
+impl<C: Command> History<C> {
     /// Returns a new history.
-    pub fn new(target: C::Target) -> Record<C> {
-        Record {
+    pub fn new(target: C::Target) -> History<C> {
+        History {
             inner: Inner::new(),
             target,
         }
